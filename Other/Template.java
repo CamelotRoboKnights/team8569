7b123eacf1f1a7241d768ac6bb0846587532acb9
Christopher Radcliffe <car97@me.com>
2016-12-04 21:10:21 -0500

    task 6893 A template was written for peogrammers to copy and add to

---
+++ Other/Template.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.team.Other;//This might need to changed to be in a differnt folder like Merlin1 or K9Robo
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CompassSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.team.Merlin1.Merlin1Hardware; //More Import statements may be needed
+
+@TeleOp(name = "Template", group = "Other")//This NEEDS to be changed tp the name of the code
+@Disabled //Uncomment this if it is not wanted on the phone
+public class Template extends LinearOpMode { //The name after public class needs to be the same as the file name
+
+
+    /* Declare OpMode members. */
+    Merlin1Hardware robot = new Merlin1Hardware();//The hardware map needs to be the hardware map of the robot we are using
+
+    @Override
+    public void runOpMode() {
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+        //init other variables.
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Say", "Hello Driver");    //It is ready to run
+        telemetry.update();//Updates and displays data on the screen.
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            //Where you write the code that you want to run
+
+
+
+
+
+            telemetry.update();
+            // Pause for metronome tick.  40 mS each cycle = update 25 times a second.
+            robot.waitForTick(40);
+        }
+    }
+
+}
