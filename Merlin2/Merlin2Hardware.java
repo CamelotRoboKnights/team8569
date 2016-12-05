63348f3a239e3158a5b97526cf992deea5da4d2d
Christopher Radcliffe <car97@me.com>
2016-12-04 21:29:59 -0500

    task 6700 The hardware map and one of the teleops has been started, there are methods for driving and the collector, the others need more work.

---
+++ Merlin2/Merlin2Hardware.java
@@ -0,0 +1,162 @@
+package org.firstinspires.ftc.teamcode.team.Merlin2;
+
+import com.kauailabs.navx.ftc.AHRS;
+import com.kauailabs.navx.ftc.navXPIDController;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cCompassSensor;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+//import com.qualcomm.robotcore.hardware.I2cDevice;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+//import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+//import com.qualcomm.robotcore.util.Range;
+//import com.qualcomm.robotcore.hardware.I2cAddr;
+//import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
+//import com.qualcomm.robotcore.hardware.I2cDeviceSynchImpl;
+//import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+
+/**
+ * This is NOT an opmode.
+ *
+ *
+ * This hardware class assumes the following device names have been configured on the robot:
+ * Note:  All names are lower case and some have single spaces between words.
+ *
+ * Motor channel:  Motor1:      "motor1"
+ * Motor channel:  Motor2:      "motor2"
+ * Motor channel:  Motor3:      "motor3"
+ * Motor channel:  Motor4:      "motor4"
+ * Motor channel:  Sweeper:     "sweeper"
+ * Motor channel:  Flipper:     "flipper"
+ *
+ * STILL NEED THE SENSORS
+ *
+ *
+ */
+public class Merlin2Hardware
+{
+    /* Public OpMode members. */
+    public DcMotor  Motor1    = null;//Front right motor
+    public DcMotor  Motor2    = null;//Front left motor
+    public DcMotor  Motor3    = null;//Back left motor
+    public DcMotor  Motor4    = null;//Back right motor
+    public DcMotor  LiftCollector   = null;//The sweeper\ball collector
+    public DcMotor  Flipper   = null;//The flipper that flips the ball
+    public OpticalDistanceSensor LeftLight;//The left Optical Distance Sensor
+    public OpticalDistanceSensor RightLight;//The right Optical Distance Sensor
+    public ModernRoboticsI2cRangeSensor LeftRange;//The range sensor on the buton press side
+    public ModernRoboticsI2cRangeSensor RightRange;
+    public ModernRoboticsI2cCompassSensor compas;//The compass Modern robotics sensor
+
+
+
+    //The navX sensor
+    public final int NAVX_DIM_I2C_PORT = 2;//Sets the NavX port
+    public AHRS navx_device;
+    public navXPIDController yawPIDController;
+
+    //public ElapsedTime runtime = new ElapsedTime();//Sets a thing called runtime that measures an elapsed time.
+
+    //More NavX
+    public final byte NAVX_DEVICE_UPDATE_RATE_HZ = 50;
+
+    public final double YAW_PID_P = 0.005;
+    public final double YAW_PID_I = 0.0;
+    public final double YAW_PID_D = 0.0;
+
+    public boolean calibration_complete = false;//Sets a boolean that says if the NavX sensor is complete or not
+
+
+
+
+    /* local OpMode members. */
+    HardwareMap hwMap           =  null; //Saying this this has no opmode
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Constructor */
+    public Merlin2Hardware(){
+
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;//Seting a reference for the hardware map
+
+
+        RightLight = hwMap.opticalDistanceSensor.get("odrSensor");//Finds the Right Optical Distance sensor in the hardware map
+        LeftLight = hwMap.opticalDistanceSensor.get("odlSensor");//Finds the Left Optical Distance sensor in the hardware map
+
+        LeftRange = hwMap.get(ModernRoboticsI2cRangeSensor.class, "RightRange");//Finds the range sensor in the hardware map
+        RightRange = hwMap.get(ModernRoboticsI2cRangeSensor.class, "LeftRange");
+        compas = hwMap.get(ModernRoboticsI2cCompassSensor.class, "compas");//Finds the compass sensor in the hardware map
+
+        navx_device = AHRS.getInstance(hwMap.deviceInterfaceModule.get("dim"), NAVX_DIM_I2C_PORT, AHRS.DeviceDataType.kProcessedData, NAVX_DEVICE_UPDATE_RATE_HZ);
+        yawPIDController = new navXPIDController( navx_device, navXPIDController.navXTimestampedDataSource.YAW);
+
+
+        // Define and Initialize Motors
+        Motor1  = hwMap.dcMotor.get("motor1");//Finds the front right motor in the hardware map
+        Motor2  = hwMap.dcMotor.get("motor2");//Finds the front left motor in the hardware map
+        Motor3  = hwMap.dcMotor.get("motor3");//Finds the back left motor in the hardware map
+        Motor4  = hwMap.dcMotor.get("motor4");//Finds the back right motor in the hardware map
+        LiftCollector = hwMap.dcMotor.get("Lift");//Finds the sweeper in the hardware map
+        Flipper = hwMap.dcMotor.get("flipper");//Finds the flipper in the hardware map
+        Motor1.setDirection(DcMotorSimple.Direction.REVERSE);//Sets the motor power to reverse so forwards is positive
+        Motor2.setDirection(DcMotorSimple.Direction.FORWARD);//Sets the motor power as positive so forward is still positive
+        Motor3.setDirection(DcMotorSimple.Direction.FORWARD);//Sets the motor power as positive so forward is still positive
+        Motor4.setDirection(DcMotorSimple.Direction.REVERSE);//Sets the motor power to reverse so forwards is positive
+        LiftCollector.setDirection(DcMotorSimple.Direction.REVERSE);//Sets the motor power to reverse so collecting is positive
+        Flipper.setDirection(DcMotorSimple.Direction.REVERSE);//Sets the Flipper to forward so launching is positive
+
+        // Set all motors to zero power
+        Motor1.setPower(0);//Sets the power to 0 so motors don't move
+        Motor2.setPower(0);
+        Motor3.setPower(0);
+        Motor4.setPower(0);
+        LiftCollector.setPower(0);
+        Flipper.setPower(0);
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        Motor1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Motor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Motor3.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Motor4.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        LiftCollector.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Flipper.setMode(DcMotor.RunMode.RUN_USING_ENCODER);//Use encoder's becuase that is used for the flicking
+
+
+
+        // Define and initialize ALL installed servos.
+
+    }
+
+    /***
+     *
+     * waitForTick implements a periodic delay. However, this acts like a metronome with a regular
+     * periodic tick.  This is used to compensate for varying processing times for each cycle.
+     * The function looks at the elapsed cycle time, and sleeps for the remaining time interval.
+     *
+     * @param periodMs  Length of wait cycle in mSec.
+     */
+    public void waitForTick(long periodMs) {
+
+        long  remaining = periodMs - (long)period.milliseconds();
+
+        // sleep for the remaining portion of the regular cycle period.
+        if (remaining > 0) {
+            try {
+                Thread.sleep(remaining);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        // Reset the cycle clock for the next pass.
+        period.reset();
+    }
+}
+
---
+++ Merlin2/Merlin2RedTeleOp.java
@@ -0,0 +1,134 @@
+package org.firstinspires.ftc.teamcode.team.Merlin2;//This might need to changed to be in a differnt folder like Merlin1 or K9Robo
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CompassSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.team.Merlin1.Merlin1Hardware; //More Import statements may be needed
+
+@TeleOp(name = "Template", group = "Other")//This NEEDS to be changed tp the name of the code
+@Disabled //Uncomment this if it is not wanted on the phone
+public class Merlin2RedTeleOp extends LinearOpMode { //The name after public class needs to be the same as the file name
+
+
+    /* Declare OpMode members. */
+    Merlin2Hardware robot = new Merlin2Hardware();//The hardware map needs to be the hardware map of the robot we are using
+
+    @Override
+    public void runOpMode() {
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+        //init other variables.
+        double TargetEncoder = 0;
+        double CurrentHeight = 0;
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Say", "Hello Driver");    //It is ready to run
+        telemetry.update();//Updates and displays data on the screen.
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            //Where you write the code that you want to run
+            feildOrentedDrive();
+            TargetEncoder = launchBall(TargetEncoder);
+            collection();
+            if(gamepad1.a){
+                wallSquare();
+            }
+            capBallLift(CurrentHeight);
+
+
+
+            telemetry.update();
+            // Pause for metronome tick.  40 mS each cycle = update 25 times a second.
+            robot.waitForTick(40);
+        }
+    }
+
+
+    public String wallSquare(){
+        return "-----";
+    }
+    public double capBallLift(double CurrentHeight) {
+
+        return CurrentHeight;
+    }
+
+    public double launchBall(double TargetEncoder) {
+        double CurrentEncoder = robot.Flipper.getCurrentPosition();
+        double OneRotation = 1650;
+
+        if(TargetEncoder - CurrentEncoder < 3){
+            robot.Flipper.setPower(0);
+            if(gamepad2.dpad_up){
+                TargetEncoder = TargetEncoder+OneRotation;
+            }
+        }
+        else{
+            robot.Flipper.setPower(.9);
+        }
+        return TargetEncoder;
+    }
+
+    public void collection(){
+        if (gamepad2.x){//if X is pressed make the spinner set to dispose of balls
+            robot.LiftCollector.setPower(-.5);
+        }
+        else if(gamepad2.b){//If B os pressed make the spinner set to collect balls
+            robot.LiftCollector.setPower(.5);
+        }
+        else if(gamepad2.a){//If A is pressed make the spinner not spin
+            robot.LiftCollector.setPower(0);
+        }
+
+    }
+
+
+    public void feildOrentedDrive(){
+        double JoyX;
+        double JoyY;
+        double NewX;
+        double NewY;
+        double pi = 3.1415926;
+        double OrientationDegrees;
+        double OrientationRadians;
+
+        JoyY = -gamepad1.left_stick_y;
+        JoyX = gamepad1.left_stick_x;
+        OrientationDegrees = robot.navx_device.getYaw();
+        OrientationRadians = OrientationDegrees * pi / 180;
+        NewY = JoyY * Math.cos(OrientationRadians) + JoyX * Math.sin(OrientationRadians);
+        NewX = -JoyY * Math.sin(OrientationRadians) + JoyX * Math.cos(OrientationRadians);
+
+        if(Math.abs(gamepad1.left_stick_y) >= 0.02 || Math.abs(gamepad1.left_stick_x) >= 0.02){
+            robot.Motor1.setPower(NewY - NewX);//Sets the motor power for the front right motor
+            robot.Motor2.setPower(NewY + NewX);//sets the motor power for the front left motor
+            robot.Motor3.setPower(NewY - NewX);//Sets the motor power for the back left motor
+            robot.Motor4.setPower(NewY + NewX);//Sets the motor power for the back right motor
+        }
+        else if(gamepad1.right_trigger >= .02){//This cancels out noise and sets the robot to turn right at the speed of the right trigger
+            robot.Motor1.setPower(-gamepad1.right_trigger);
+            robot.Motor2.setPower(gamepad1.right_trigger);
+            robot.Motor3.setPower(gamepad1.right_trigger);
+            robot.Motor4.setPower(-gamepad1.right_trigger);
+        }
+        else if(gamepad1.left_trigger >= .02){//This cancels out noise and sets the robot to turn left at the speed of the left trigger
+            robot.Motor1.setPower(gamepad1.left_trigger);
+            robot.Motor2.setPower(-gamepad1.left_trigger);
+            robot.Motor3.setPower(-gamepad1.left_trigger);
+            robot.Motor4.setPower(gamepad1.left_trigger);
+        }
+        else{//If none of these are true make the robot not move
+            robot.Motor1.setPower(0);
+            robot.Motor2.setPower(0);
+            robot.Motor3.setPower(0);
+            robot.Motor4.setPower(0);
+        }
+    }
+}
