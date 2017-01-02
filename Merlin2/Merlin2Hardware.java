c6fa5841303412b023c44238a6768325b7d98ce0
Christopher Radcliffe <car97@me.com>
2017-01-02 10:32:28 -0500

    task 7454 The hardware map was modified slightly to be correct

--- Merlin2/Merlin2Hardware.java
+++ Merlin2/Merlin2Hardware.java
@@ -44,6 +44,7 @@ public class Merlin2Hardware
     public DcMotor  Motor4    = null;//Back right motor
     public DcMotor  LiftCollector   = null;//The sweeper\ball collector
     public DcMotor  Flipper   = null;//The flipper that flips the ball
+    public DcMotor Lift = null;
     public OpticalDistanceSensor LeftLight;//The left Optical Distance Sensor
     public OpticalDistanceSensor RightLight;//The right Optical Distance Sensor
     public ModernRoboticsI2cRangeSensor LeftRange;//The range sensor on the buton press side
@@ -53,7 +54,7 @@ public class Merlin2Hardware
 
 
     //The navX sensor
-    public final int NAVX_DIM_I2C_PORT = 2;//Sets the NavX port
+    public final int NAVX_DIM_I2C_PORT = 3;//Sets the NavX port
     public AHRS navx_device;
     public navXPIDController yawPIDController;
 
@@ -89,9 +90,8 @@ public class Merlin2Hardware
         RightLight = hwMap.opticalDistanceSensor.get("odrSensor");//Finds the Right Optical Distance sensor in the hardware map
         LeftLight = hwMap.opticalDistanceSensor.get("odlSensor");//Finds the Left Optical Distance sensor in the hardware map
 
-        LeftRange = hwMap.get(ModernRoboticsI2cRangeSensor.class, "RightRange");//Finds the range sensor in the hardware map
-        RightRange = hwMap.get(ModernRoboticsI2cRangeSensor.class, "LeftRange");
-        compas = hwMap.get(ModernRoboticsI2cCompassSensor.class, "compas");//Finds the compass sensor in the hardware map
+        LeftRange = hwMap.get(ModernRoboticsI2cRangeSensor.class, "LeftRange");//Finds the range sensor in the hardware map
+        RightRange = hwMap.get(ModernRoboticsI2cRangeSensor.class, "RightRange");
 
         navx_device = AHRS.getInstance(hwMap.deviceInterfaceModule.get("dim"), NAVX_DIM_I2C_PORT, AHRS.DeviceDataType.kProcessedData, NAVX_DEVICE_UPDATE_RATE_HZ);
         yawPIDController = new navXPIDController( navx_device, navXPIDController.navXTimestampedDataSource.YAW);
@@ -102,14 +102,16 @@ public class Merlin2Hardware
         Motor2  = hwMap.dcMotor.get("motor2");//Finds the front left motor in the hardware map
         Motor3  = hwMap.dcMotor.get("motor3");//Finds the back left motor in the hardware map
         Motor4  = hwMap.dcMotor.get("motor4");//Finds the back right motor in the hardware map
-        LiftCollector = hwMap.dcMotor.get("Lift");//Finds the sweeper in the hardware map
+        LiftCollector = hwMap.dcMotor.get("collector");//Finds the sweeper in the hardware map
         Flipper = hwMap.dcMotor.get("flipper");//Finds the flipper in the hardware map
+        Lift = hwMap.dcMotor.get("lift");
         Motor1.setDirection(DcMotorSimple.Direction.REVERSE);//Sets the motor power to reverse so forwards is positive
         Motor2.setDirection(DcMotorSimple.Direction.FORWARD);//Sets the motor power as positive so forward is still positive
         Motor3.setDirection(DcMotorSimple.Direction.FORWARD);//Sets the motor power as positive so forward is still positive
         Motor4.setDirection(DcMotorSimple.Direction.REVERSE);//Sets the motor power to reverse so forwards is positive
         LiftCollector.setDirection(DcMotorSimple.Direction.REVERSE);//Sets the motor power to reverse so collecting is positive
         Flipper.setDirection(DcMotorSimple.Direction.REVERSE);//Sets the Flipper to forward so launching is positive
+        Lift.setDirection(DcMotorSimple.Direction.REVERSE);
 
         // Set all motors to zero power
         Motor1.setPower(0);//Sets the power to 0 so motors don't move
@@ -118,6 +120,7 @@ public class Merlin2Hardware
         Motor4.setPower(0);
         LiftCollector.setPower(0);
         Flipper.setPower(0);
+        Lift.setPower(0);
 
         // Set all motors to run without encoders.
         // May want to use RUN_USING_ENCODERS if encoders are installed.
@@ -127,6 +130,7 @@ public class Merlin2Hardware
         Motor4.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         LiftCollector.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         Flipper.setMode(DcMotor.RunMode.RUN_USING_ENCODER);//Use encoder's becuase that is used for the flicking
+        Lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
 
 
